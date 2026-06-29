796765ecbb8a0991c43dcdeb21c18b87aa01a145
Ongkiboy <rizqi30syahrulm@gmail.com>
2026-06-29 19:35:17 +0700

    feat(middleware):mendaftarkan middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'cek.admin' => \App\Http\Middleware\cekAdmin::class,
+            'cek.mahasiswa' => \App\Http\Middleware\cekMahasiswa::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
