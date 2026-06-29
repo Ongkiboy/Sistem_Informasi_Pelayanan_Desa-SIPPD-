09e5344b938e3217fd83147b29cf89e1c1271541
Ongkiboy <rizqi30syahrulm@gmail.com>
2026-06-29 19:41:44 +0700

    feat(route): daftarkan auth ke routes

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -1,3 +1,11 @@
 <?php
 // routes/web/auth.php — Assignee: Iki
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/login',[AuthController::class, 'tampilkanLogin'])->name('login');
+Route::post('/login',[AuthController::class,'prosesLogin'])->name('login.proses');
+
+Route::post('/logout',[AuthController::class,'logout'])
+    ->name('logout')
+    ->middleware('auth');
